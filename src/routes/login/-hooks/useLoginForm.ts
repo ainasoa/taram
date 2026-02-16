048b5bb5be7855c73e9b8cc7001de7620afd7315
MkRica <ainasoaricajob@gmail.com>
2026-02-16 08:42:50 +0300

    fix: update navigation path after successful login

--- src/routes/login/-hooks/useLoginForm.ts
+++ src/routes/login/-hooks/useLoginForm.ts
@@ -36,7 +36,7 @@ export default function useLoginForm() {
       
             if (error) throw error;
       
-            await navigate({ to: "/protected" });
+            await navigate({ to: "/properties" });
             
         } catch (error) {
             setSubmitError(true)
